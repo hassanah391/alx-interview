7deb57a4e3d92ea371df59dd7d80dfea15c03fe8
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-07-19 20:51:36 +0300

    Add pascal_traiangle()

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+pascal_triangle
+"""
+import math
+
+
+def pascal_triangle(n):
+    """return a list of lists of integers representing
+    the Pascal’s triangle of n"""
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = [math.comb(i, k) for k in range(i + 1)]
+        triangle.append(row)
+
+    return triangle
